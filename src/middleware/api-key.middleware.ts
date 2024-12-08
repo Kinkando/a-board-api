20511254afc2a3d16f21a0e1485e18782937c996
Kinkando <TY_Sage@hotmail.com>
2024-12-08 19:47:08 +0700

    feat: add api key middleware

---
+++ src/middleware/api-key.middleware.ts
@@ -0,0 +1,16 @@
+import { HttpStatus, Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+
+@Injectable()
+export class ApiKeyMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    const apiKey = process.env.API_KEY;
+    const apiKeyReq = req.headers['X-Api-Key'];
+    if (!apiKeyReq || apiKeyReq !== apiKey) {
+      return res
+        .status(HttpStatus.UNAUTHORIZED)
+        .json({ error: 'api key is invalid' });
+    }
+    next();
+  }
+}
