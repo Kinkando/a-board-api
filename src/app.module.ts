1df6ff30c923de8b1d22c3fe5653970967855022
Kinkando <TY_Sage@hotmail.com>
2024-12-08 21:16:01 +0700

    chore(post): init post controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,13 +7,20 @@ import { AuthenService } from './authen/authen.service';
 import { JwtService } from './jwt/jwt.service';
 import { DatabaseModule } from './module/database.module';
 import { ApiKeyMiddleware } from './middleware/api-key.middleware';
+import { PostController } from './post/post.controller';
+import { PostService } from './post/post.service';
 import { UserController } from './user/user.controller';
 import { UserService } from './user/user.service';
 
 @Module({
   imports: [ConfigModule.forRoot(), DatabaseModule],
-  controllers: [AppController, AuthenController, UserController],
-  providers: [AppService, AuthenService, JwtService, UserService],
+  controllers: [
+    AppController,
+    AuthenController,
+    UserController,
+    PostController,
+  ],
+  providers: [AppService, AuthenService, JwtService, UserService, PostService],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
