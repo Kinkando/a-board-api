a77b98d216183b46c6dbd79ab257fdae5036fc9f
Kinkando <TY_Sage@hotmail.com>
2024-12-08 20:51:42 +0700

    chore(app): add validation pipe and CORS on application layer

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
+import { ValidationError } from 'class-validator';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      skipMissingProperties: true,
+      exceptionFactory: (errors: ValidationError[]) => {
+        return new BadRequestException({
+          error: errors.map((err) => {
+            delete err.target;
+            delete err.children;
+            return err;
+          }),
+        });
+      },
+    }),
+  );
+  app.enableCors({
+    origin: true,
+    methods: 'GET,POST,PUT,PATCH,DELETE,OPTIONS',
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
