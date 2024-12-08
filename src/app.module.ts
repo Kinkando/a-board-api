89ddc8b5ab24b2b6b81f216d2d2b743fd23df936
Kinkando <TY_Sage@hotmail.com>
2024-12-08 20:49:29 +0700

    chore: register api key middleware on login route

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -6,10 +6,15 @@ import { AuthenController } from './authen/authen.controller';
 import { AuthenService } from './authen/authen.service';
 import { JwtService } from './jwt/jwt.service';
 import { DatabaseModule } from './module/database.module';
+import { ApiKeyMiddleware } from './middleware/api-key.middleware';
 
 @Module({
   imports: [ConfigModule.forRoot(), DatabaseModule],
   controllers: [AppController, AuthenController],
   providers: [AppService, AuthenService, JwtService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(ApiKeyMiddleware).forRoutes('/auth/login');
+  }
+}
