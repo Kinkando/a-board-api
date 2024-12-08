b13575f25ef73c3dc62a275116ce80330ada9787
Kinkando <TY_Sage@hotmail.com>
2024-12-08 21:03:58 +0700

    chore(test): add unit test get user

---
+++ src/user/user.controller.spec.ts
@@ -0,0 +1,64 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { UserController } from './user.controller';
+import { UserService } from './user.service';
+import { ConfigModule } from '@nestjs/config';
+import {
+  GatewayTimeoutException,
+  INestApplication,
+  NotFoundException,
+} from '@nestjs/common';
+import { JwtService } from '../jwt/jwt.service';
+import { v7 } from 'uuid';
+
+describe('UserController', () => {
+  let app: INestApplication;
+  let userService: jest.Mocked<UserService>;
+  let controller: UserController;
+
+  beforeEach(async () => {
+    userService = {
+      getUser: jest.fn(),
+    } as any;
+
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [ConfigModule.forRoot()],
+      controllers: [UserController],
+      providers: [
+        JwtService,
+        {
+          provide: UserService,
+          useValue: userService,
+        },
+      ],
+    }).compile();
+
+    controller = module.get<UserController>(UserController);
+    app = module.createNestApplication();
+    await app.init();
+  });
+
+  it('get user success', async () => {
+    const user = {
+      id: v7(),
+      profileImageUrl: null,
+      username: 'tester',
+    };
+    userService.getUser.mockResolvedValue(user);
+    const response = await controller.getUser({ userId: user.id });
+    expect(response).toEqual(user);
+  });
+
+  it('get user failure not found', async () => {
+    userService.getUser.mockRejectedValue(new NotFoundException());
+    expect(
+      async () => await controller.getUser({ userId: 'not-found' }),
+    ).rejects.toThrow();
+  });
+
+  it('get user failure gateway timeout', async () => {
+    userService.getUser.mockRejectedValue(new GatewayTimeoutException());
+    expect(
+      async () => await controller.getUser({ userId: 'invalid' }),
+    ).rejects.toThrow();
+  });
+});
---
+++ src/user/user.service.spec.ts
@@ -0,0 +1,62 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { UserService } from './user.service';
+import { ConfigModule } from '@nestjs/config';
+import { Kysely } from 'kysely';
+import { v7 } from 'uuid';
+import { NotFoundException } from '@nestjs/common';
+
+describe('UserService', () => {
+  let service: UserService;
+  let dbMock: jest.Mocked<Kysely<any> & { executeTakeFirst: jest.Mock }>;
+
+  beforeEach(async () => {
+    dbMock = {
+      selectFrom: jest.fn().mockReturnThis(),
+      select: jest.fn().mockReturnThis(),
+      where: jest.fn().mockReturnThis(),
+      executeTakeFirst: jest.fn(),
+    } as any;
+
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [ConfigModule.forRoot()],
+      providers: [UserService, { provide: 'DATABASE', useValue: dbMock }],
+    }).compile();
+
+    service = module.get<UserService>(UserService);
+  });
+
+  it('get user success', async () => {
+    const mockUser = { id: v7(), username: 'test' };
+
+    dbMock.executeTakeFirst.mockResolvedValue(mockUser);
+
+    const user = await service.getUser(mockUser.id);
+
+    expect(user).toBeDefined();
+    expect(user).toBe(mockUser);
+    expect(user.username).toBe(mockUser.username);
+
+    expect(dbMock.selectFrom).toHaveBeenCalledWith('user');
+    expect(dbMock.executeTakeFirst).toHaveBeenCalled();
+  });
+
+  it('get user failed: user is not found', async () => {
+    dbMock.executeTakeFirst.mockResolvedValue(undefined);
+
+    expect(async () => await service.getUser(v7())).rejects.toThrow(
+      new NotFoundException(),
+    );
+
+    expect(dbMock.selectFrom).toHaveBeenCalledWith('user');
+    expect(dbMock.executeTakeFirst).toHaveBeenCalled();
+  });
+
+  it('get user failed: database error', async () => {
+    dbMock.executeTakeFirst.mockRejectedValue(new Error('database error'));
+
+    expect(async () => await service.getUser(v7())).rejects.toThrow();
+
+    expect(dbMock.selectFrom).toHaveBeenCalledWith('user');
+    expect(dbMock.executeTakeFirst).toHaveBeenCalled();
+  });
+});
